f93e3d1a94e47a98c4ce934e31c5102e1f5b0a6a
Manish Jindal <manish.jin@gmail.com>
2016-09-05 22:15:45 +0200

    DB column case test

--- app/helpers/companies_helper.rb
+++ app/helpers/companies_helper.rb
@@ -1,7 +1,6 @@
 module CompaniesHelper
 	def tablereport(driverid, companyid)
-		@reporttable = Shipment.where("company_id =#{companyid} and \"Driver_ID\" = #{driverid}")
+		@reporttable = Shipment.where("company_id =#{companyid} and \Driver_ID\ = #{driverid}")
 	end
 end
-
 #http://stackoverflow.com/questions/3902432/rails-and-heroku-pgerror-column-does-not-exist
